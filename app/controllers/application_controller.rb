0a2eaedfaa2e69010c34b21d6ed8f9212478828e
Joaquín de Antueno <joaquindea@hotmail.es>
2019-11-29 18:42:53 -0300

    Commit inicial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :require_content_type
+  after_action :add_content_type
+
+  def require_content_type
+    if request.content_type != "application/vnd.api+json"
+      head :unsupported_media_type
+    end
+  end
+
+  def add_content_type
+    response.set_header("Content-Type", "application/vnd.api+json")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'admin', password:'admin')
+
+Product.create(code: 'ABC123', description: 'Galletitas Portenitas', detail: 'Lorem ipsum dolor sit amen', price: 25.5)
+Product.create(code: 'ABC124', description: 'Pepas de la abuela', detail: 'Lorem ipsum dolor sit amen', price: 20.5)
+Product.create(code: 'ABC125', description: '9 de Oro', detail: 'Lorem ipsum dolor sit amen', price: 30.5)
+Product.create(code: 'ABC126', description: 'Bay Biscuit', detail: 'Lorem ipsum dolor sit amen', price: 35.5)
+
+Item.create(product: Produc)
+
+IvaCondition.create(name: 'IVA Responsable Inscripto')
+IvaCondition.create(name: 'IVA Responsable no Inscripto')
+IvaCondition.create(name: 'IVA no Responsable')
+IvaCondition.create(name: 'IVA Sujeto Exento')
+IvaCondition.create(name: 'Consumidor Final')
+IvaCondition.create(name: 'Responsable Monotributo')
+IvaCondition.create(name: 'Sujeto no Categorizado')
+IvaCondition.create(name: 'Proveedor del Exterior')
+IvaCondition.create(name: 'Cliente del Exterior')
+IvaCondition.create(name: 'IVA Liberado – Ley Nº 19.640')
+IvaCondition.create(name: 'IVA Responsable Inscripto – Agente de Percepción')
+IvaCondition.create(name: 'Pequeño Contribuyente Eventual')
+IvaCondition.create(name: 'Monotributista Social')
+IvaCondition.create(name: 'Pequeño Contribuyente Eventual Social')
