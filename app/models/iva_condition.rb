6c0bc3b6bafcf6fe546dd3bbdcdccf419c50b659
Joaquín de Antueno <joaquindea@hotmail.es>
2020-01-24 17:17:23 -0300

    Validaciones en iva y telefono

--- app/models/iva_condition.rb
+++ app/models/iva_condition.rb
@@ -1,3 +1,5 @@
 class IvaCondition < ApplicationRecord
   has_many :clients
+
+  validates :name, presence: true, uniqueness: { case_sensitive: true }
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,5 @@
 class Phone < ApplicationRecord
   belongs_to :client
+
+  validates :number, presence: true, uniqueness: true
 end
